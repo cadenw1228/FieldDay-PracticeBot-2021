882a56911587526868d8214d1afbfee808530b3d
Will <718230@dpsk12.net>
2021-12-02 11:46:14 -0800

    first commit

--- src/main/java/frc/robot/Subsystems/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -1,12 +1,23 @@
 package frc.robot.Subsystems;
 
+import edu.wpi.first.wpilibj.Spark;
 import frc.robot.Framework.Subsystem;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.PWMSparkMax;
 
 public class Shooter implements Subsystem {
     
+    Joystick controlla = new Joystick(0);
+    
+    PWMSparkMax shoo = new PWMSparkMax(6);
+    TalonSRX s_lr = new TalonSRX(8);
     
     public void robotInit(){
-        System.out.println("Shooter init");
+        
+        
     }
 
     public void robotPeriodic(){
@@ -14,6 +25,7 @@ public class Shooter implements Subsystem {
     }
 
     public void autonomousInit(){
+    
 
     }
     public void autonomousPeriodic(){
@@ -21,10 +33,30 @@ public class Shooter implements Subsystem {
     }
 
     public void teleopInit(){
-
+       
     }
 
     public void teleopPeriodic(){
+        if(controlla.getRawButton(5) == true){
+            System.out.println("OKAY");
+            s_lr.set(ControlMode.PercentOutput, .2); 
+        }
+        else if(controlla.getRawButton(6)){
+            s_lr.set(ControlMode.PercentOutput, -.2);
+        }
+        else{
+            s_lr.set(ControlMode.PercentOutput, 0);
+
+        }
+        System.out.println(controlla.getRawAxis(3));
+        if(controlla.getRawAxis(4)>= .1){
+            shoo.set(.2);
+        }
+        else{
+            shoo.set(0);
+        }
+            
+
         
     }
 }
